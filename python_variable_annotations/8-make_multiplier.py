80959f9c9e0f0a24218857a5a77b3a0069b952f1
Swagtiago29 <sant29ramos@gmail.com>
2024-12-05 21:07:59 -0300

    fix

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -15,7 +15,7 @@ Example usage:
 """
 
 
-def make_multipier(multiplier: float) -> Callable[[float], float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     This function creates a multiplier function.
 
