1d58ccd0f2b7ff78c37546f1ddb57e3f6fa95838
Swagtiago29 <sant29ramos@gmail.com>
2024-12-05 21:21:13 -0300

    -

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python3
-from typing import Callable
 
 """
-This module provides a function for creating multiplier functions.
+8-make_multiplier.py
 
-It includes the function `make_multiplier`, which takes a float `multiplier`
-as an argument and returns a new function that multiplies its input by this
-multiplier. The resulting function can be used to perform the multiplication
-on different values.
+This module provides a function, `make_multipier`, that generates a multiplier
+function. The multiplier function multiplies its input by a specified multipli
+value provided when creating it.
 
-The module demonstrates the use of higher-order functions in Python, where
-a function returns another function. The returned function "remembers" the
-`multiplier` value it was created with and applies it when called.
+The module demonstrates the concept of higher-order functions, where one
+function returns another function, and the returned function "remembers" the
+multiplier value.
+
+Functions:
+    make_multipier(multiplier: float) -> Callable[[float], float]:
+        Creates and returns a function that multiplies its input by the multiplier.
 
 Example usage:
-    multiply_by_3 = make_multiplier(3.0)
+    multiply_by_3 = make_multipier(3.0)
     result = multiply_by_3(4.0)  # result will be 12.0
-    multiply_by_5 = make_multilpier(5.0)
+    multiply_by_5 = make_multipier(5.0)
     result2 = multiply_by_5(4.0)  # result2 will be 20.0
-
-This module is useful for creating custom multiplier functions dynamically
-based on a given multiplier value.
 """
 
+from typing import Callable
 
-def make_multiplier(multiplier: float) -> Callable[[float], float]:
+
+def make_multipier(multiplier: float) -> Callable[[float], float]:
     """
-    Creates a function that multiplies a given number by a specified
-    multiplier.
+    Creates a function that multiplies a given number by a specified multiplie
 
     Args:
         multiplier (float): The multiplier value that will be used to multiply
