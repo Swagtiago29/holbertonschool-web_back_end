3f75512cd6d1a35a7e3cabb33b9f26a4ee1552f7
Swagtiago29 <sant29ramos@gmail.com>
2024-12-08 17:08:27 -0300

    fix 2 errors

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 import importlib
 import time
+import asyncio
 
 wait_n = importlib.import_module('1-concurrent_coroutines').wait_n
 
 def measure_time(n: int, max_delay:int) -> float:
-    runtime = time.time()
+    startint_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    ending_time = time.time()
+    runtime = ending_time - startint_time
     return runtime / n
