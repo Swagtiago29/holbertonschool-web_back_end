6c7367a1cd3224dfff80c63b645c57754044d0b4
Swagtiago29 <sant29ramos@gmail.com>
2024-12-09 11:35:43 -0300

    add 0 and README

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Module: Random Integer Generator with Asynchronous Delays
+--------------------------------------------------------
+
+This module defines an asynchronous function `async_generator` that simulates random delays
+while generating random integers. The function generates 10 random integers between 0 and 10,
+with each integer generation followed by a random delay (in seconds). The results are returned
+in a list.
+
+Usage:
+    This script is intended to be run as a standalone program.
+    The main function runs the async generator and prints the results.
+
+Functions:
+    - async_generator(): Asynchronously generates random integers with random delays.
+    - main(): Test function that runs the async_generator and prints the result.
+
+Example:
+    To run the script and view the output, execute the module in an environment
+    that supports asynchronous programming.
+"""
+import asyncio
+from random import randint
+
+
+async def async_generator() -> list:
+    """
+    Asynchronously generates a list of 10 random integers (between 0 and 10),
+    each followed by a random delay. The delay is simulated using
+    asyncio.sleep().
+
+    This function does not block the event loop, and it allows other tasks
+    to run while the delays are in place.
+
+    Returns:
+        list: A list of 10 randomly generated integers (between 0 and 10).
+    """
+    results = []
+    for _ in range(10):
+        x = randint(0, 10)
+        await asyncio.sleep(x)
+        results.append(x)
+    return results  
---
+++ python_async_comprehension/README.md
@@ -0,0 +1,7 @@
+<h1>Python - Async Comprehension</h1>
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+<ul>
+    <li>How to write an asynchronous generator</li>
+    <li>How to use async comprehensions</li>
+    <li>How to type-annotate generators</li>
+</ul>
