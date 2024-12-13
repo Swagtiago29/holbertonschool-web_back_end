55c3ea518adfe4a14850e2fee35418321f15a7df
Swagtiago29 <sant29ramos@gmail.com>
2024-12-12 22:46:58 -0300

    add task 1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -29,4 +29,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            pass
+            assert type(page) == int and type(page_size) == int
+            assert page > 0 and page_size > 0
+            start_row, end_row = index_range(page, page_size)
+            dataset = self.dataset()
+            return dataset[start_row : end_row]
