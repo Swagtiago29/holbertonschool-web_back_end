74416b03da23e46f421e1bb2318c445b12ed33f6
Swagtiago29 <sant29ramos@gmail.com>
2024-12-15 19:01:07 -0300

    9 doc

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+"""
+Module containing the insert_school() function
+"""
+
 
 def insert_school(mongo_collection, **kwargs):
     """
-    Inserts a new document into the given MongoDB collection based on provided keyword arguments.
+    Inserts a new document into the given MongoDB collection based on
+    provided keyword arguments.
     """
     ins = mongo_collection.insert_one(kwargs)
     return ins.inserted_id
