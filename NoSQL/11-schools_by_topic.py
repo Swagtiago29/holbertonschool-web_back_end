3db69c93040fc641034762ba9672921b3a69eaa4
Swagtiago29 <sant29ramos@gmail.com>
2024-12-15 19:39:40 -0300

    11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
-
+"""
+Module that contains schools_by_topic()
+"""
 
 def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of school having a specific topic.
+    """
     a = mongo_collection.find({"topic": topic})
     return a
