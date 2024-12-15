2c3b069e2b5499d27c4085449a571125581118ad
Swagtiago29 <sant29ramos@gmail.com>
2024-12-15 13:41:26 -0300

    fix baby names

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -12,7 +12,7 @@ from typing import Dict
 class Server:
     """Server class to paginate a database of popular baby names.
     """
-    DATA_FILE = "pagination/Popular_Baby_Names.csv"
+    DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
