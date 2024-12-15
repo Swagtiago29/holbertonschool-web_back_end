a6c5ad5ee60fbedcf32ee0705a374c9829848916
Swagtiago29 <sant29ramos@gmail.com>
2024-12-15 18:32:10 -0300

    add 8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/en python3
+"""
+    Module with the function list_all
+"""
+from pymongo import MongoClient
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in the given MongoDB collection.
+    """
+    all_docs = list(mongo_collection.find())
+    return all_docs
