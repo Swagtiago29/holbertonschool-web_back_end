0c6e5b12475791cc3cc64463aaf8bc1dcd1f2e36
Swagtiago29 <sant29ramos@gmail.com>
2025-07-17 16:44:11 -0300

    fix 3

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """lru_cache.py"""
-BaseCaching = __import__('base_caching').BaseCaching
 from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
@@ -9,20 +9,29 @@ class LRUCache(BaseCaching):
     LRU (Least Recently Used)
     """
     def __init__(self):
+        """Initialize the cache with an empty OrderedDict."""
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        if key is None or item is None: 
+        """
+        Add an item in the cache.
+        """
+        if key is None or item is None:
             return None
-        if key not in self.cache_data and len(self.cache_data) >= self.MAX_ITEMS:
+        if key not in self.cache_data and \
+                len(self.cache_data) >= self.MAX_ITEMS:
             first_item = next(iter(self.cache_data))
             self.cache_data.pop(first_item)
             print(f"DISCARD: {first_item}")
 
         self.cache_data[key] = item
         self.cache_data.move_to_end(key)
+
     def get(self, key):
+        """
+        Retrieve an item by key.
+        """
         if key is None or key not in self.cache_data:
             return None
         self.cache_data.move_to_end(key)
