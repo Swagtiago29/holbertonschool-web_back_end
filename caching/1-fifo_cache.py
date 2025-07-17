b195b4db292d6526e378603cf90e83740587be8d
Swagtiago29 <sant29ramos@gmail.com>
2025-07-17 15:07:52 -0300

    add 1 fix

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -3,20 +3,29 @@
 BaseCaching = __import__('base_caching').BaseCaching
 
 class FIFOCache(BaseCaching):
+    """
+    FIFOCache is a caching system that implements FIFO (First-In, First-Out) eviction.
+    """
+
     def __init__(self):
-        self.MAX_ITEMS = BaseCaching.MAX_ITEMS
         super().__init__()
+
     def put(self, key, item):
+        """
+        Add an item in the cache.
+        If the cache is full, discard the first item put in (FIFO).
+        """
         if key is None or item is None:
-            pass
-        if len(self.cache_data) >= self.MAX_ITEMS:
+            return
+        
+        if key not in self.cache_data and len(self.cache_data) >= self.MAX_ITEMS:
             for keys in self.cache_data:
                 first_item = keys
                 break
-            self.cache_data.pop(first_item, None)
+            self.cache_data.pop(first_item)
             print(f"DISCARD: {first_item}" )
+
         self.cache_data[key] = item
+
     def get(self, key):
-        if key is not None and key in self.cache_data:
-            return self.cache_data.get(key)
-        return None
+        return self.cache_data.get(key, None)
