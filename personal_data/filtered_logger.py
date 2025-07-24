96c8d97f48a9f02de66564903b70b8e697dab2cc
Swagtiago29 <sant29ramos@gmail.com>
2025-07-24 14:48:34 -0300

    0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    """" comments fpr filter_datum"""
+    """ comments fpr filter_datum """
     return re.sub(rf"({'|'.join(fields)})=[^{separator}]*",
-                  rf"\1={redaction}", message)
+                  rf"\1={redaction}",
+                  message)
