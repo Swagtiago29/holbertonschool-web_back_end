f7017e2db09be5e5c6f9ea05979a33a078c5ea0a
Swagtiago29 <sant29ramos@gmail.com>
2025-07-24 15:28:18 -0300

    a

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,7 +3,10 @@
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List[str],
+                 redaction: str, message: str, separator: str) -> str:
     """Redacts values of specified fields in a log message."""
-    return re.sub(rf"({'|'.join(map(re.escape, fields))})=.*?{re.escape(separator)}",
-                  rf"\1={redaction}{separator}", message)
+    return re.sub(
+        rf"({'|'.join(map(re.escape, fields))})=.*?{re.escape(separator)}",
+        rf"\1={redaction}{separator}", message)
