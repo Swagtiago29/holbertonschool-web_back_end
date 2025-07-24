6df5b2967cdbcc2d67fa4025526eb1809545da84
Swagtiago29 <sant29ramos@gmail.com>
2025-07-24 14:45:42 -0300

    add 0 fix

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+"""Task 0"""
 import re
 
-def filter_datum(fields: list, redaction: str, message:str, separator: str) -> str:
-    return re.sub(rf"({'|'.join(fields)})=[^{separator}]*", rf"\1={redaction}", message)
+
+def filter_datum(fields, redaction, message, separator):
+    return re.sub(rf"({'|'.join(fields)})=[^{separator}]*",
+                  rf"\1={redaction}", message)
