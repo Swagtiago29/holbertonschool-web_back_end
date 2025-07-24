172a9f502a44c284ab0bf130a1c825f1445ae6c1
Swagtiago29 <sant29ramos@gmail.com>
2025-07-24 15:25:05 -0300

    adddd

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-"""Task 0"""
+"""Task 0 - log obfuscator"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str],
+                 redaction: str, message: str, separator: str):
     """Redacts values of specified fields in a log message."""
-    return re.sub(rf"({'|'.join(fields)})=[^{separator}]*",
-                  rf"\1={redaction}", message)
+    return re.sub(f"({'|'.join(fields)})=.*?{separator}",
+                  f"\\1={redaction}{separator}", message)
