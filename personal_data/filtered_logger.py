8b526d81dca7adbdf53e0204fdf01e55b09b0816
Swagtiago29 <sant29ramos@gmail.com>
2025-07-25 13:16:28 -0300

    1add

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""Task 0 - log obfuscator"""
+"""Task 1 - Log formatter"""
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str],
@@ -10,3 +11,18 @@ def filter_datum(fields: List[str],
     return re.sub(
         rf"({'|'.join(map(re.escape, fields))})=.*?{re.escape(separator)}",
         rf"\1={redaction}{separator}", message)
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        return super().format(record)
