71282cc81dd9e8d14eeb42f1891466fd341e9539
Swagtiago29 <sant29ramos@gmail.com>
2025-07-26 13:34:47 -0300

    add 6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -2,6 +2,10 @@
 """encrypt password.py for task 5"""
 import bcrypt
 
+def is_valid(hashed_password: bytes, password:str) -> bool:
+    """Checks if a plaintext password matches the hashed password."""
+    return bcrypt.checkpw(password.encode(), hashed_password)
+
 
 def hash_password(password: str) -> bytes:
     """Returns the hashed password in bytes"""
