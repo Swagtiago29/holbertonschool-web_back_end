267853cd4da17108fe26a6f14c29e49f3654d9ba
Swagtiago29 <sant29ramos@gmail.com>
2025-07-27 17:21:44 -0300

    done!

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -69,7 +69,7 @@ class BasicAuth(Auth):
 
     def current_user(self, request=None) -> TypeVar('User'):
         """overloads Auth and retrieves the User instance for a request"""
-        auth_header = self.authorization_header()
+        auth_header = self.authorization_header(request)
         if auth_header is None:
             return None
         b64_auth = self.extract_base64_authorization_header(auth_header)
