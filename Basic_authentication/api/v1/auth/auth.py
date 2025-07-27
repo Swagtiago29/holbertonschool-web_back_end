3b985f4c4cad0d2686e22edf906a38ef4416c386
Swagtiago29 <sant29ramos@gmail.com>
2025-07-27 13:45:37 -0300

    test

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ from flask import request
 from typing import List, TypeVar
 
 class Auth:
+    """Auth Class methods"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """to be implemented"""
