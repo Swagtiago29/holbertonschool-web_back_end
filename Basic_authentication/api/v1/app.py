4bc9c18d50c7be1cd71b0948f5d901cdcbcf5dfc
Swagtiago29 <sant29ramos@gmail.com>
2025-07-27 15:24:24 -0300

    5 done

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -17,18 +17,22 @@ if getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+
 @app.before_request
 def handle_before_request():
     """Handles all authentication before each request"""
     if auth is None:
         return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
 
     if auth.require_auth(request.path, excluded_paths):
         if auth.authorization_header(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
