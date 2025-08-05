1f38f5c12f1355ec1033375e3ea756f1bdac9acf
Swagtiago29 <sant29ramos@gmail.com>
2025-08-05 11:33:37 -0300

    test

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -31,7 +31,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> object:
+    def add_user(self, email: str, hashed_password: str) -> User:
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self.__session.commit()
