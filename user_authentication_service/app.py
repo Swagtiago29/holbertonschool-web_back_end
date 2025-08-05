533035eeff18ff43536955368f856f93ff977b44
Swagtiago29 <sant29ramos@gmail.com>
2025-08-05 14:31:50 -0300

    xd

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -19,7 +19,7 @@ def users():
         AUTH.register_user(email, password)
         return {"email": f"{email}", "message": "user created"}, 200
     except ValueError:
-        return {"message": "email already registered"}, 500
+        return {"message": "email already registered"}, 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
