90053ab06a0a3acbcc07dbef53cdc826e67d0533
Swagtiago29 <sant29ramos@gmail.com>
2025-08-05 14:50:54 -0300

    ...

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -10,16 +10,19 @@ app = Flask(__name__)
 def welcome():
     return jsonify({"message": "Bienvenue"})
 
+
 @app.route('/users', methods=['POST'])
 def users():
     data = request.get_json() or request.form
     email = data.get('email')
     password = data.get('password')
+
     try:
         AUTH.register_user(email, password)
         return jsonify({"email": email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
