46ba5c0179b647003d88baddefe26f26d7244336
Swagtiago29 <sant29ramos@gmail.com>
2025-08-05 11:47:30 -0300

    test 2

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,8 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import User
-
 from user import Base
 
 
@@ -36,3 +37,12 @@ class DB:
         self._session.add(new_user)
         self.__session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first
+            if user is None:
+                raise NoResultFound
+            return user
+        except InvalidRequestError:
+            raise
