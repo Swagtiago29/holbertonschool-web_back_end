596f962e4c3cd731ebf5263c43bc0dc6fb832e71
Swagtiago29 <sant29ramos@gmail.com>
2025-08-07 20:28:37 -0300

    test 12

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -48,3 +48,12 @@ class Auth:
             return user_uuid
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        try:
+            if not session_id:
+                return None
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
