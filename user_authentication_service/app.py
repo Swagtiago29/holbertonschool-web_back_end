ae728fff4a9b658f780d896cc744693ef281d417
Swagtiago29 <sant29ramos@gmail.com>
2025-08-07 20:04:42 -0300

    11 done

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -40,7 +40,7 @@ def login():
 
     session_id = AUTH.create_session(email)
     response = jsonify({"email": email, "message": "logged in"})
-    response.set_cookie({'session_id': session_id})
+    response.set_cookie('session_id', session_id)
     return response
 
 
