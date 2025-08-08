280f091021f4ccf51bb3ab9fe8ff55df6b0c4ee9
Swagtiago29 <sant29ramos@gmail.com>
2025-08-08 14:11:34 -0300

    add 18

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -76,3 +76,14 @@ class Auth:
             return new_uuid
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed = _hash_password(password)
+            user.hashed_password = hashed
+            user.reset_token = None
+            self._db._session.commit()
+            return None
+        except NoResultFound:
+            raise ValueError
