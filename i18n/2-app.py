a575abe9170a08ddf82579cbbb0e6480f0d8f0d4
Swagtiago29 <sant29ramos@gmail.com>
2025-08-17 13:32:39 -0300

    test 2

---
+++ i18n/2-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Doc for a small flask app for i18n project"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """class containing Connfig data"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+def get_locale():
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+babel.init_app(get_locale())
+
+@app.route("/", methods=["GET"])
+def index():
+    """Function to return template on route=/ """
+    return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
