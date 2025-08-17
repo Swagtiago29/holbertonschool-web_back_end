7613bcb59167538075c18e8503ed11f0344933f8
Swagtiago29 <sant29ramos@gmail.com>
2025-08-17 13:04:44 -0300

    add 0 fix

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
-from flask import flask, jsonify
+from flask import flask, render_template
 
 app = flask(__name__)
+
+
 @app.route("/", methods=["GET"])
 def hello():
-    return jsonify("hello"), 200
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
