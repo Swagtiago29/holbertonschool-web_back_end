13a5228a48a50ce39cc9a1834c92af4192ca0bfc
Swagtiago29 <sant29ramos@gmail.com>
2025-08-17 13:08:58 -0300

    0 done

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-from flask import flask, render_template
+from flask import Flask, render_template
 
-app = flask(__name__)
+app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
-def hello():
-    return render_template("index.html")
+def index():
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
