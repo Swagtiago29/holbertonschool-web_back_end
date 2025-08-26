5793109aa5d946e84e231a5d643981e1d1f5e41c
Swagtiago29 <sant29ramos@gmail.com>
2025-08-26 14:38:34 -0300

    3 test

---
+++ unittests_in_js/3-payment.js
@@ -0,0 +1,5 @@
+function sendPaymentRequestsToApi(totalAmount, totalShipping) {
+    const result = Utils.calculateNumber('SUM', totalAmount, totalShipping)
+    console.log('The total is: ', result)
+}
+module.exports = sendPaymentRequestsToApi;
---
+++ unittests_in_js/3-payments.test.js
@@ -0,0 +1,14 @@
+const { expect } = requre('chai')
+const sinon = require('sinon')
+const Utils = requre('./utils')
+const sendPaymentRequestsToApi = require('./3-payment')
+
+describe('sendPaymentRequestsToApi', function(){
+    it('should call Utils.calculateNumber with SUM, 100, 20', function(){
+        const spy = sinon.spy(Utils, 'calculateNumber');
+        sendPaymentRequestsToApi('SUM', 100, 20);
+        expect(spy.calledOnce).to.be.true;
+        expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+        spy.restore();
+    })
+})
