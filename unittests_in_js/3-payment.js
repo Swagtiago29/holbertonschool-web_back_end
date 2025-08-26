f439ebe3996575219626657b255da31d990c803e
Swagtiago29 <sant29ramos@gmail.com>
2025-08-26 14:54:04 -0300

    test

--- unittests_in_js/3-payment.js
+++ unittests_in_js/3-payment.js
@@ -1,6 +1,6 @@
 const Utils = require('./utils')
 function sendPaymentRequestsToApi(totalAmount, totalShipping) {
     const result = Utils.calculateNumber('SUM', totalAmount, totalShipping)
-    console.log('The total is: ', result)
+    console.log(`The total is: ${result}`)
 }
 module.exports = sendPaymentRequestsToApi;
