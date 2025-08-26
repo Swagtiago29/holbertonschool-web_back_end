bf05d395fb67ea3da006a03e073b038fe551abc9
Swagtiago29 <sant29ramos@gmail.com>
2025-08-26 13:42:34 -0300

    test 2

---
+++ unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,29 @@
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul');
+
+describe('calculateNumber', function () {
+    describe('type SUM', function () {
+        it('should round the numbers and add them', function () {
+            expect(calculateNumber('SUM', 2.2, 1.1)).to.equal(3);
+        });
+        it('should round the numbers and add them', function () {
+            expect(calculateNumber('SUM', 2.7, 3.7)).to.equal(5)
+        })
+    })
+    describe('type SUBTRACT', function () {
+        it('should round the numbers and subtract them', function () {
+            expect(calculateNumber('SUBTRACT', 9.7, 4.7)).to.equal(5)
+        });
+        it('should round the numbers and subtract them', function () {
+            expect(calculateNumber('SUBTRACT', 6.2, 3.2)).to.equal(3)
+        })
+    })
+    describe('type DIVIDE', function () {
+        it('should round the numbers and divide them', function () {
+            expect(calculateNumber('DIVIDE', 9.7, 4.7)).to.equal(2)
+        });
+        it('should round the numbers and subtract them', function () {
+            expect(calculateNumber('DIVIDE', 6.2, 3.2)).to.equal(2)
+        })
+    })
+});
