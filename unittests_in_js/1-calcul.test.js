7c837e34a7ecc77bd77bd44740beff28d494f870
Swagtiago29 <sant29ramos@gmail.com>
2025-08-26 13:15:16 -0300

    test

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -6,23 +6,23 @@ describe('calculateNumber', function () {
         it('should round the numbers and add them', function () {
             assert.strictEqual(calculateNumber('SUM', 2.2, 1.1), 3)
         });
-        it('should round the numbers and add them', function() {
+        it('should round the numbers and add them', function () {
             assert.strictEqual(calculateNumber('SUM', 2.7, 3.7), 7)
         })
     })
-    describe('type SUBTRACT', function() {
-        it('should round the numbers and subtract them', function() {
+    describe('type SUBTRACT', function () {
+        it('should round the numbers and subtract them', function () {
             assert.strictEqual(calculateNumber('SUBTRACT', 9.7, 4.7), 5)
         });
-        it('should round the numbers and subtract them', function() {
+        it('should round the numbers and subtract them', function () {
             assert.strictEqual(calculateNumber('SUBTRACT', 6.2, 3.2), 3)
         })
     })
-    describe('type DIVIDE', function() {
-        it('should round the numbers and divide them', function() {
+    describe('type DIVIDE', function () {
+        it('should round the numbers and divide them', function () {
             assert.strictEqual(calculateNumber('DIVIDE', 9.7, 4.7), 2)
         });
-        it('should round the numbers and subtract them', function() {
+        it('should round the numbers and subtract them', function () {
             assert.strictEqual(calculateNumber('DIVIDE', 6.2, 3.2), 2)
         })
     })
