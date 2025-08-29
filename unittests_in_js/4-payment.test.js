77ef792012cc19b12f86ed4eca956b7d3e9dd2c8
Swagtiago29 <sant29ramos@gmail.com>
2025-08-29 11:52:30 -0300

    fix typo

--- unittests_in_js/4-payment.test.js
+++ unittests_in_js/4-payment.test.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai')
 const sinon = require('sinon')
 const Utils = require('./utils')
-const sendPaymentRequestsToApi = require('./3-payment')
+const sendPaymentRequestsToApi = require('./4-payment')
 
 describe('sendPaymentRequestsToApi', function () {
 
