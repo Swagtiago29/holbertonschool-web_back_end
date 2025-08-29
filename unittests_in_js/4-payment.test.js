e2360d1124c31e564a21af29e722c60d9b262642
Swagtiago29 <sant29ramos@gmail.com>
2025-08-29 11:39:32 -0300

    test 4

---
+++ unittests_in_js/4-payment.test.js
@@ -0,0 +1,19 @@
+const { expect } = require('chai')
+const sinon = require('sinon')
+const Utils = require('./utils')
+const sendPaymentRequestsToApi = require('./3-payment')
+
+describe('sendPaymentRequestsToApi', function(){
+    it('should call Utils.calculateNumber with SUM, 100, 20', function(){
+        const spy = sinon.spy(Utils, 'calculateNumber');
+        const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+
+        sendPaymentRequestsToApi(100, 20);
+        
+        expect(stub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+        expect(spy.calledOnceWithExactly('The total is: 10')).to.be.true;
+
+        stub.restore();
+        spy.restore();
+    })
+})
