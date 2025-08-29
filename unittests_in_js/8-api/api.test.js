727f84afcfd9f434b2ba93e5813fcf3b92881f63
Swagtiago29 <sant29ramos@gmail.com>
2025-08-29 14:01:36 -0300

    add 8

---
+++ unittests_in_js/8-api/api.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const { expect } = require('chai');
+const app = require('./api');
+
+describe('Index page', function () {
+    it('should return correct status code', function (done) {
+        request(app)
+            .get('/')
+            .expect(200, done)
+    });
+
+    it('should return correct result', function (done) {
+        request(app)
+            .get('/')
+            .end((err, res) => {
+                expect(res.text).to.equal('Welcome to the payment system');
+                done();
+            });
+    });
+
+    it('should return the correct content type', function (done) {
+        request(app)
+            .get('/')
+            .expect('Content-Type', /html/)
+            .end(done)
+    })
+})
