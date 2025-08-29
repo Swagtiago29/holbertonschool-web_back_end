072167edcf6c81e70b07b2e478fc5c4280fc8fe0
Swagtiago29 <sant29ramos@gmail.com>
2025-08-29 14:24:01 -0300

    test

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,19 +1,6 @@
 const { expect } = require('chai');
-const app = require('./api');
-
-let server;
 
 describe('Index page', function () {
-  before((done) => {
-    // Start server before tests
-    server = app.listen(7865, done);
-  });
-
-  after((done) => {
-    // Close server after tests
-    server.close(done);
-  });
-
   it('should return correct status code and body', async function () {
     const res = await fetch('http://localhost:7865/');
     const text = await res.text();
