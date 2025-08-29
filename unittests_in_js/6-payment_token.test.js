3592cae80df2c70e4161ce0bed51d0a9d6f4ffad
Swagtiago29 <sant29ramos@gmail.com>
2025-08-29 13:21:56 -0300

    add 6

---
+++ unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,13 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', function () {
+    it('should return succesful response qhen success is true', function (done) {
+        getPaymentTokenFromAPI(true)
+            .then((response) => {
+                expect(response).to.deep.equal({ data: 'Successful response from the API' });
+                done();
+            })
+            .catch((err) => done(err))
+    })
+})
